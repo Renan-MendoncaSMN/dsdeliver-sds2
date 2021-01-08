ea7883ba52963304ee871212f8869c90eb1b80c3
Renan Mendonca <renan.mendonca@smn.com.br>
2021-01-08 11:45:23 -0300

    Finish front web

--- front-web/src/Orders/types.tsx
+++ front-web/src/Orders/types.tsx
@@ -6,8 +6,16 @@ export type Product = {
     imageUri: string;
 }
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
     latitude: number;
     longitude: number;
     address: string;
 }
+
+type ProductId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProductId[];
+} & OrderLocationData; //junta os atributos do OrderLocationdata com o OrderPayload
